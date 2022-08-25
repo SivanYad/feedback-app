77fe556014a8a141911726c1ff127eddb1514a4a
unknown <justsivan@gmail.com>
2022-08-25 13:04:49 +0300

    added FeedbackItem

---
+++ src/components/FeedbackItem.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function FeedbackItem() {
+  return (
+    <div className='card'>
+      <div className='num-display'>10</div>
+      <div className="text-dispaly">This is an example of feedback item</div>
+    </div>
+  );
+}
+
+export default FeedbackItem;
