4e6a50c48f8bcecc3c8f0207515aeeeac496375d
unknown <justsivan@gmail.com>
2022-08-28 11:43:22 +0300

    added conditional styling

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
+import Card from './shared/Card';
 import PropTypes from 'prop-types';
 import { useState } from 'react';
 
 function FeedbackItem({ item }) {
 
   return (
-    <div className='card'>
+    <Card className='card' reverse={true} >
       <div className='num-display'>{item.rating}</div>
       <div className='text-dispaly'>{item.text}</div>
       
-    </div>
+    </Card>
   );
 }
 
