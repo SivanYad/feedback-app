858cfcff9008d6159a346dc19f358adf2cdb93f7
unknown <justsivan@gmail.com>
2022-08-30 09:23:09 +0300

    added animation to FeedbackList

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,4 +1,4 @@
-import { } from 'framer-motion'
+import { motion, AnimatePresence } from 'framer-motion';
 import React from 'react';
 import FeedbackItem from './FeedbackItem';
 import PropTypes from 'prop-types';
@@ -9,22 +9,32 @@ function FeedbackList({ feedback, handleDelete }) {
   }
   return (
     <div className='feedback-list'>
-      {feedback.map((item) => (
-        <FeedbackItem
+      <AnimatePresence>
+        {feedback.map((item) => (
+          <motion.div 
           key={item.id}
-          item={item}
-          handleDelete={handleDelete}
-        />
-      ))}
+          initial={{opacity: 0}}
+          animate={{opacity: 1}}
+          exit={{opacity: 0}}
+          >
+            <FeedbackItem
+              key={item.id}
+              item={item}
+              handleDelete={handleDelete}
+            />
+          </motion.div>
+        ))}
+      </AnimatePresence>
     </div>
   );
+ 
 }
 
 // FeedbackList.propTypes = {
 //   feedback: PropTypes.arrayOf(
 //     // PropTypes.shape({
-      
-//     //   text: PropTypes.string.isRequired,
+
+//     //  ext: PropTypes.string.isRequired,
 //     //   rating: PropTypes.number.isRequired,
 //     // })
 //   ),
