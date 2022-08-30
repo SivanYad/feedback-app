599dbf586a32b53dec8832df2c0e92a7daf2bcec
unknown <justsivan@gmail.com>
2022-08-30 09:52:20 +0300

    Added react-router

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Card from '../components/shared/Card'
+
+function AboutPage() {
+  return (
+    <Card>
+        <div className="about">
+            <h1>About This Project</h1>
+            <p>This is a React app to leave feedback for a project or service</p>
+            <p>Version 1.0.0</p>
+            <p></p>
+        </div>
+    </Card>
+  )
+}
+
+export default AboutPage
